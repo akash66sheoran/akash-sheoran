5f10a614e644c8012420bc0270b132e79ac1a927
Akash <akash66sheoran@gmail.com>
2022-07-13 16:33:07 +0530

    added about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me.png'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { AiOutlineFundProjectionScreen } from 'react-icons/ai'
 
 const About = () => {
     return (
         <section id='about'>
-            About
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about_container">
+                <div className="about_me">
+                    <div className="about_me-image">
+                        <img src={ME} alt="About image" />
+                    </div>
+                </div>
+
+                <div className="about_content">
+                    <div className="about_cards">
+                        <article className='about_card'>
+                            <FaAward className='about_icon' />
+                            <h5>Experience</h5>
+                            <small>Fresher</small>
+                        </article>
+
+                        <article className='about_card'>
+                            <FiUsers className='about_icon' />
+                            <h5>Clients</h5>
+                            <small>5+ Worldwide</small>
+                        </article>
+
+                        <article className='about_card'>
+                            <AiOutlineFundProjectionScreen className='about_icon' />
+                            <h5>Projects</h5>
+                            <small>2+ completed</small>
+                        </article>
+                    </div>
+
+                    <p>An enthusiastic MERN stack web developer currently pursuing BTech degree 4th year in computer science engineering.</p>
+
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
         </section>
     )
 }
