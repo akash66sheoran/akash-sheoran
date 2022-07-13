462ba256bf00396722ab58bf40798fa16294be6b
Akash <akash66sheoran@gmail.com>
2022-07-13 15:06:23 +0530

    initial commit with header and navbar

---
+++ src/components/header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/myself.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container header_container">
+                <h5>Hello I'm</h5>
+                <h1>Akash Sheoran</h1>
+                <h5 className='text-light'>MERN Stack Developer</h5>
+                <CTA />
+
+                <HeaderSocials />
+
+                <div className="me">
+                    <img src={ME} alt="me" />
+                </div>
+
+                <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/header/HeaderSocials.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsLinkedin, BsInstagram } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+
+const HeaderSocials = () => {
+    return (
+        <div className="header_socials">
+            <a href="https://linkedin.com" target='_blank' rel="noreferrer" ><BsLinkedin /></a>
+            <a href="https://github.com" target='_blank' rel="noreferrer" ><FaGithub /></a>
+            <a href="https://instagram.com" target='_blank' rel="noreferrer" ><BsInstagram /></a>
+        </div>
+    )
+}
+
+export default HeaderSocials
