cb97f99d4091ee74ebe7b1e195038ce8afea1804
Akash <akash66sheoran@gmail.com>
2022-07-14 10:01:38 +0530

    add send email/message functionality

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
 import { BsWhatsapp } from 'react-icons/bs'
 import { AiFillPhone } from 'react-icons/ai'
+import emailjs from 'emailjs-com'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_d4dsvdt', 'template_lepn1k6', form.current, 'ez_AqLo_51_4mnfRV')
+
+        e.target.reset()
+
+        toast.success("Email sent successfully", {
+            position: "top-center",
+            theme: "colored"
+        })
+
+    };
+
     return (
         <section id="contact">
             <h5>Get In Touch</h5>
@@ -31,13 +51,14 @@ const Contact = () => {
                         <a href="tel:916239982589" target='_blank' rel="noreferrer">Make a call</a>
                     </article>
                 </div>
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Enter your name' required />
                     <input type="email" name='email' placeholder='Enter your email' required />
                     <textarea name="message" rows="7" placeholder='Your message' required></textarea>
                     <button type="submit" className='btn btn-primary'>Send Message</button>
                 </form>
             </div>
+            <ToastContainer />
         </section>
     )
 }
